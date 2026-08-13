afb9b449c9c29cd2a9d963dddf5466009b688bf3
marcosroberioribeiro-create <marcosroberioribeiro@gmail.com>
2026-08-13 10:14:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-st.set_page_config(page_title="Kero Fish - Peixe e CamarÃ£o", layout="wide")
+st.set_page_config(page_title="Kero Fish - Peixe e Camar\u00e3o", layout="wide")
 
 # Exibe a foto oficial do logo
 st.sidebar.image("Screenshot_20260813_084749_WhatsApp.jpg", use_container_width=True)
 
 opcao = st.sidebar.radio(
-    "NavegaÃ§Ã£o",
-    ["InÃ­cio", "Cadastro", "Estoque / ProduÃ§Ã£o", "Vendas", "Financeiro"]
+    "Navega\u00e7\u00e3o",
+    ["In\u00edcio", "Cadastro", "Estoque / Produ\u00e7\u00e3o", "Vendas", "Financeiro"]
 )
 
 st.sidebar.markdown("---")
@@ -15,21 +15,21 @@ st.sidebar.markdown("**Contatos:**\n- (85) 98502-6019\n- (85) 99277-6984")
 st.sidebar.markdown("**Instagram:** @kerofish")
 
 # Tela de InÃ­cio
-if opcao == "InÃ­cio":
-    st.title("Kero Fish - Peixe e CamarÃ£o")
-    st.write("Sistema de GestÃ£o Integrada")
+if opcao == "In\u00edcio":
+    st.title("Kero Fish - Peixe e Camar\u00e3o")
+    st.write("Sistema de Gest\u00e3o Integrada")
     
     col1, col2, col3 = st.columns(3)
-    col1.metric("Vendas do MÃªs", "R$ 0,00")
+    col1.metric("Vendas do M\u00eas", "R$ 0,00")
     col2.metric("Estoque Atual", "0 kg")
     col3.metric("Clientes Ativos", "0")
 
 # Tela de Cadastro
 elif opcao == "Cadastro":
     st.title("Cadastro Geral")
-    tipo = st.selectbox("O que deseja cadastrar?", ["Cliente", "Produto (Peixe / CamarÃ£o)", "Fornecedor"])
+    tipo = st.selectbox("O que deseja cadastrar?", ["Cliente", "Produto (Peixe / Camar\u00e3o)", "Fornecedor"])
     
-    nome = st.text_input("Nome / DescriÃ§Ã£o")
+    nome = st.text_input("Nome / Descri\u00e7\u00e3o")
     telefone = st.text_input("Telefone / Contato")
     
     if st.button("Salvar Cadastro"):
